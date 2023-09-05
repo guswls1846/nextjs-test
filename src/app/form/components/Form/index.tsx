d1a9ec4265221baa8bf835c91a89b2e9552b00b0
oil <guswls1846@naver.com>
2023-09-05 23:10:42 +0900

    update: Form resolver 추가

--- src/app/form/components/Form/index.tsx
+++ src/app/form/components/Form/index.tsx
@@ -2,32 +2,46 @@
 
 import { useForm, SubmitHandler } from "react-hook-form";
 import tw from "twin.macro";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-enum GenderEnum {
-  female = "female",
-  male = "male",
-  other = "other",
-}
+const schema = z.object({
+  firstName: z.string().min(10),
+  gender: z.string().nonempty({ message: "Gender is required" }),
+});
 
-interface FormInput {
-  firstName: string;
-  gender: GenderEnum;
-}
+type FormSchema = z.infer<typeof schema>;
 
 export function Form() {
-  const { register, handleSubmit } = useForm<FormInput>();
-  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchema>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormSchema> = (data) => console.log(data);
 
   return (
     <FormWrap onSubmit={handleSubmit(onSubmit)}>
       <Label>First Name</Label>
       <Input {...register("firstName")} />
+      {errors.firstName?.message && (
+        <ErrorMessage>{errors.firstName?.message}</ErrorMessage>
+      )}
+
       <Label>Gender Selection</Label>
       <Select {...register("gender")}>
+        <option value="">Select...</option>
         <option value="female">female</option>
         <option value="male">male</option>
         <option value="other">other</option>
       </Select>
+      {errors.gender?.message && (
+        <ErrorMessage>{errors.gender?.message}</ErrorMessage>
+      )}
+
       <Input type="submit" />
     </FormWrap>
   );
@@ -37,3 +51,4 @@ const FormWrap = tw.form`flex flex-col gap-4`;
 const Label = tw.label`text-xl`;
 const Input = tw.input`border border-gray-400 rounded-md p-2 text-black`;
 const Select = tw.select`border border-gray-400 rounded-md p-2 text-black`;
+const ErrorMessage = tw.p`text-red-500`;
