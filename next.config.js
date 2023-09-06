0725f826b5d5544bbe76c933dfd4f6d855ba08ef
oil <guswls1846@naver.com>
2023-09-06 18:34:09 +0900

    chore: pageExtensions 설정

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const withTwin = require("./withTwin.js");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+};
 
 module.exports = withTwin(nextConfig);
