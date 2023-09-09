3b56051c3c68f569357ac2ffd3dff3b001d147c9
oil <guswls1846@naver.com>
2023-09-09 16:53:33 +0900

    chore: pageExtensions 비활성화

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withTwin = require("./withTwin.js");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  // pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 module.exports = withTwin(nextConfig);
