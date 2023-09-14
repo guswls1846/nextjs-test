3af83bf8376aa6edfeb43b9a44e8a4e1d782462e
oil <guswls1846@naver.com>
2023-09-14 21:52:40 +0900

    feat: +,- 버튼 구현

--- src/app/counter/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const CounterPage = () => {
-  const [count, setCount] = useState(0);
-  return <h3 data-testid={"counter"}>{count}</h3>;
-};
-
-export default CounterPage;
---
+++ src/app/counter/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useState } from "react";
+
+const CounterPage = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3 data-testid={"counter"}>{count}</h3>
+      <button data-testid={"minus-button"} onClick={() => setCount(count - 1)}>
+        -
+      </button>
+      <button data-testid={"plus-button"} onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </div>
+  );
+};
+
+export default CounterPage;
