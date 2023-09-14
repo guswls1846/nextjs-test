d02eda1dddf0b9db9ad06a5338ed5b68b8ad79b1
lane4-oil <oil@lane4.ai>
2023-09-14 22:46:03 +0900

    update: on/off 버튼 클릭시 +,- 버튼 disabled 속성 상태값 변경

--- src/app/counter/page.tsx
+++ src/app/counter/page.tsx
@@ -3,22 +3,29 @@ import { useState } from "react";
 
 const CounterPage = () => {
   const [count, setCount] = useState(0);
+  const [onOff, setOnOff] = useState(false);
   return (
     <div>
       <h3 data-testid={"counter"}>{count}</h3>
       <button
         data-testid={"minus-button"}
         onClick={() => setCount((count) => count - 1)}
+        disabled={onOff}
       >
         -
       </button>
       <button
         data-testid={"plus-button"}
         onClick={() => setCount((count) => count + 1)}
+        disabled={onOff}
       >
         +
       </button>
-      <button data-testid={"on-off-button"} style={{ backgroundColor: "blue" }}>
+      <button
+        data-testid={"on-off-button"}
+        style={{ backgroundColor: "blue" }}
+        onClick={() => setOnOff(!onOff)}
+      >
         on/off
       </button>
     </div>
