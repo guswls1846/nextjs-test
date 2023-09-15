1173368e0854a97409f86543f497881edea80799
oil <guswls1846@naver.com>
2023-09-16 00:00:55 +0900

    refact: 쿼리 우선순위와 userEvent를 고려한 테스트 코드 작성

--- __test__/app/counter.page.test.tsx
+++ __test__/app/counter.page.test.tsx
@@ -1,54 +1,57 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Counter from "@/app/counter/page";
 
 describe("Counter 페이지 테스트", () => {
+  const user = userEvent.setup();
+
   it("카운터 페이지 렌더링", () => {
     render(<Counter />);
-    const CounterElement = screen.getByTestId("counter");
+    const CounterElement = screen.getByRole("heading");
     expect(CounterElement).toHaveTextContent("0");
   });
 
   it("마이너스 버튼", () => {
     render(<Counter />);
-    const minusButton = screen.getByTestId("minus-button");
+    const minusButton = screen.getByRole("button", { name: "-" });
     expect(minusButton).toHaveTextContent("-");
   });
 
   it("플러스 버튼", () => {
     render(<Counter />);
-    const plusButton = screen.getByTestId("plus-button");
+    const plusButton = screen.getByRole("button", { name: "+" });
     expect(plusButton).toHaveTextContent("+");
   });
 
-  it("마이너스 버튼 클릭시, count -1 ", () => {
+  it("마이너스 버튼 클릭시, count -1 ", async () => {
     render(<Counter />);
-    const minusButton = screen.getByTestId("minus-button");
-    const CounterElement = screen.getByTestId("counter");
-    fireEvent.click(minusButton);
+    const minusButton = screen.getByRole("button", { name: "-" });
+    const CounterElement = screen.getByRole("heading");
+    await user.click(minusButton);
     expect(CounterElement).toHaveTextContent("-1");
   });
 
-  it("플러스 버튼 클릭시, count 1 ", () => {
+  it("플러스 버튼 클릭시, count 1 ", async () => {
     render(<Counter />);
-    const plusButton = screen.getByTestId("plus-button");
-    const CounterElement = screen.getByTestId("counter");
-    fireEvent.click(plusButton);
+    const plusButton = screen.getByRole("button", { name: "+" });
+    const CounterElement = screen.getByRole("heading");
+    await user.click(plusButton);
     expect(CounterElement).toHaveTextContent("1");
   });
 
   it("on/off 버튼 컬러는 파란색입니다. ", () => {
     render(<Counter />);
-    const onOffButton = screen.getByTestId("on-off-button");
+    const onOffButton = screen.getByRole("button", { name: "on/off" });
     expect(onOffButton).toHaveStyle("background-color: blue");
   });
 
-  it("on/off 버튼 클릭시 +,- 버튼 disabled 속성 변경 ", () => {
+  it("on/off 버튼 클릭시 +,- 버튼 disabled 속성 변경 ", async () => {
     render(<Counter />);
-    const onOffButton = screen.getByTestId("on-off-button");
-    const minusButton = screen.getByTestId("minus-button");
-    const plusButton = screen.getByTestId("plus-button");
-    fireEvent.click(onOffButton);
+    const onOffButton = screen.getByRole("button", { name: "on/off" });
+    const minusButton = screen.getByRole("button", { name: "-" });
+    const plusButton = screen.getByRole("button", { name: "+" });
+    await user.click(onOffButton);
     expect(minusButton).toBeDisabled();
     expect(plusButton).toBeDisabled();
   });
