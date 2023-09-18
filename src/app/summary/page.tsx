a618b2223e3e482d57d648732fab02545ba466e4
oil <guswls1846@naver.com>
2023-09-18 22:56:35 +0900

    feat: 주문 확인 페이지 작성

--- src/app/summary/page.tsx
+++ src/app/summary/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import React from "react";
 
 const SummaryPage = () => {
-  return <div>주문 요약 페이지</div>;
+  const [isChecked, setIsChecked] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+  return (
+    <form>
+      <h1>주문 확인</h1>
+      <input
+        type="checkbox"
+        id="confirm-checkbox"
+        checked={isChecked}
+        onChange={handleChange}
+      />
+      <label htmlFor="confirm-checkbox">
+        주문 내역을 확인하고 결제에 동의합니다.
+      </label>
+      <br />
+      <button type="submit">결제하기</button>
+    </form>
+  );
 };
 
 export default SummaryPage;
